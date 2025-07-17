ea62168255b6b7f6f70239a64d07e18db9b91ece
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-17 16:51:55 +0800

    feat: finish the memory dealer.

---
+++ src/memory.cpp
@@ -0,0 +1,60 @@
+#include "memory.hpp"
+
+Memory &Memory::getInstance() {
+  static Memory instance;
+  return instance;
+}
+
+unsigned int Memory::load(unsigned int address) { return memory_[address]; }
+
+void Memory::store(unsigned int address, unsigned int content) {
+  memory_[address] = content;
+}
+
+LSDealer &LSDealer::getInstance() {
+  static LSDealer instance;
+  return instance;
+}
+
+unsigned int LSDealer::load(Control::MEMControlInfo::MemSize size, unsigned int address,
+                  bool extend) {
+  unsigned int result = Memory::getInstance().load(address);
+  int origin_bits;
+  switch (size) {
+  case Control::MEMControlInfo::MemSize::BYTE:
+    origin_bits = 8;
+    result &= 0xff;
+    break;
+  case Control::MEMControlInfo::MemSize::HALF:
+    origin_bits = 16;
+    result &= 0xffff;
+    break;
+  case Control::MEMControlInfo::MemSize::WORD:
+    origin_bits = 32;
+    break;
+  default:
+    throw std::exception();
+  }
+  if (extend) {
+    result = result << 32 - origin_bits;
+    result = int(result) >> origin_bits;
+  }
+  return result;
+}
+
+void LSDealer::store(Control::MEMControlInfo::MemSize size, unsigned int address,
+           unsigned int data) {
+  switch (size) {
+  case Control::MEMControlInfo::MemSize::BYTE:
+    data &= 0xff;
+    break;
+  case Control::MEMControlInfo::MemSize::HALF:
+    data &= 0xffff;
+    break;
+  case Control::MEMControlInfo::MemSize::WORD:
+    break;
+  default:
+    throw std::exception();
+  }
+  Memory::getInstance().store(address, data);
+}
---
+++ src/memory.hpp
@@ -0,0 +1,29 @@
+#ifndef MEM_HPP
+#define MEM_HPP
+#include "control.hpp"
+
+const int MEMORY_SIZE = 65536;
+
+class Memory {
+private:
+  unsigned int memory_[MEMORY_SIZE];
+  Memory() = default;
+
+public:
+  static Memory &getInstance();
+  unsigned int load(unsigned int address);
+  void store(unsigned int address, unsigned int content);
+};
+
+class LSDealer {
+private:
+  LSDealer();
+
+public:
+  LSDealer &getInstance();
+  unsigned int load(Control::MEMControlInfo::MemSize size, unsigned int address,
+                    bool extend);
+  void store(Control::MEMControlInfo::MemSize size, unsigned int address,
+             unsigned int data);
+};
+#endif
