0bc54fe5f2a2b1d94110c08b08bf85f23fb09da3
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-17 10:52:28 +0800

    feat: basic structure for control.

--- src/control.cpp
+++ src/control.cpp
@@ -1,63 +1,59 @@
 #include "control.hpp"
 #include <cassert>
 
-Instruction::Instruction() { this->command_ = 0; }
-Instruction::Instruction(unsigned int command) { this->command_ = command; }
+Instruction::Instruction() { command_ = 0; }
+Instruction::Instruction(unsigned int command) { command_ = command; }
 
-std::pair<char, Instruction::Name> Instruction::getType() {
-  return std::pair<char, Instruction::Name>(type_, name_);
-}
-std::pair<unsigned int, unsigned int> Instruction::getRegs() {
-  return std::pair<unsigned int, unsigned int>(register1_, register2_);
-}
-unsigned int Instruction::getRd() { return rd_; }
-int Instruction::getImmdiate() { return immediate_; }
-void Instruction::parse() {
-  operand_ = command_ & 0x7f;
+Control::AllControlInfo Instruction::parse() {
+  Control::AllControlInfo result;
+  result.exe_control.pc = pc_next_;
+  unsigned int operand_ = command_ & 0x7f;
   switch (operand_) {
   case 0x33: {
-    type_ = 'R';
-    rd_ = (command_ >> 7) & 0x1f;
-    register1_ = (command_ >> 15) & 0x1f;
-    register2_ = (command_ >> 20) & 0x1f;
-    func3_ = (command_ >> 12) & 0x7;
-    func7_ = (command_ >> 25) & 0x7f;
+    // For type 'R':
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.reg_control.register2 = (command_ >> 20) & 0x1f;
+    result.exe_control.signImmediate = false;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
+    unsigned int func7_ = (command_ >> 25) & 0x7f;
     switch (func3_) {
     case 0x0:
       if (func7_ == 0x00) {
-        name_ = Name::ADD;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::ADD;
       } else if (func7_ == 0x20) {
-        name_ = Name::SUB;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::SUB;
       } else {
         assert(false);
       }
       break;
     case 0x1:
-      name_ = Name::SLL;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::SLL;
       break;
     case 0x2:
-      name_ = Name::SLT;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LT;
       break;
     case 0x3:
-      name_ = Name::SLTU;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LTU;
       break;
     case 0x4:
-      name_ = Name::XOR;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::XOR;
       break;
     case 0x5:
       if (func7_ == 0x00) {
-        name_ = Name::SRL;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::SRL;
       } else if (func7_ == 0x20) {
-        name_ = Name::SRA;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::SRA;
       } else {
         assert(false);
       }
       break;
     case 0x6:
-      name_ = Name::OR;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::OR;
       break;
     case 0x7:
-      name_ = Name::AND;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::AND;
       break;
     default:
       assert(false);
@@ -65,43 +61,45 @@ void Instruction::parse() {
     break;
   }
   case 0x13: {
-    type_ = 'I';
-    rd_ = (command_ >> 7) & 0x1f;
-    register1_ = (command_ >> 15) & 0x1f;
-    func3_ = (command_ >> 12) & 0x7;
-    immediate_ = int(command_) >> 20;
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.exe_control.signImmediate = true;
+    result.exe_control.immdiate = int(command_) >> 20;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
+    unsigned int func7_ = (command_ >> 25) & 0x7f;
     switch (func3_) {
     case 0x0:
-      name_ = Name::ADDI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::ADD;
       break;
     case 0x1:
-      name_ = Name::SLLI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::SLL;
       break;
     case 0x2:
-      name_ = Name::SLTI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LT;
       break;
     case 0x3:
-      name_ = Name::SLTIU;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LTU;
       break;
     case 0x4:
-      name_ = Name::XORI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::XOR;
       break;
     case 0x5:
       func7_ = (command_ >> 27) & 0x1f;
-      immediate_ = immediate_ & 0x1f;
+      result.exe_control.immdiate = result.exe_control.immdiate & 0x1f;
       if (func7_ == 0x0) {
-        name_ = Name::SRLI;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::SRL;
       } else if (func7_ == 0x8) {
-        name_ = Name::SRAI;
+        result.exe_control.type = Control::EXEControlInfo::CalcType::SRA;
       } else {
         assert(false);
       }
       break;
     case 0x6:
-      name_ = Name::ORI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::OR;
       break;
     case 0x7:
-      name_ = Name::ANDI;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::AND;
       break;
     default:
       assert(false);
@@ -109,26 +107,33 @@ void Instruction::parse() {
     break;
   }
   case 0x3: {
-    type_ = 'I';
-    rd_ = (command_ >> 7) & 0x1f;
-    register1_ = (command_ >> 15) & 0x1f;
-    func3_ = (command_ >> 12) & 0x7;
-    immediate_ = int(command_) >> 20;
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.exe_control.signImmediate = true;
+    result.exe_control.immdiate = int(command_) >> 20;
+    result.mem_control.type = Control::MEMControlInfo::MemType::LOAD;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
     switch (func3_) {
     case 0x0:
-      name_ = Name::LB;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::BYTE;
+      result.mem_control.extend_sign = true;
       break;
     case 0x1:
-      name_ = Name::LH;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::HALF;
+      result.mem_control.extend_sign = true;
       break;
     case 0x2:
-      name_ = Name::LW;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::WORD;
+      result.mem_control.extend_sign = true;
       break;
     case 0x4:
-      name_ = Name::LBU;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::BYTE;
+      result.mem_control.extend_sign = false;
       break;
     case 0x5:
-      name_ = Name::LHU;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::HALF;
+      result.mem_control.extend_sign = false;
       break;
     default:
       assert(false);
@@ -136,36 +141,37 @@ void Instruction::parse() {
     break;
   }
   case 0x67: {
-    type_ = 'I';
-    rd_ = (command_ >> 7) & 0x1f;
-    register1_ = (command_ >> 15) & 0x1f;
-    func3_ = (command_ >> 12) & 0x7;
-    immediate_ = int(command_) >> 20;
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.exe_control.signImmediate = true;
+    result.exe_control.immdiate = int(command_) >> 20;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
     if (func3_ == 0x0) {
-      name_ = Name::JALR;
+      result.exe_control.jump = Control::EXEControlInfo::JumpType::JALR;
     } else {
       assert(false);
     }
     break;
   }
   case 0x23: {
-    type_ = 'S';
-    func3_ = (command_ >> 12) & 0x7;
-    register1_ = (command_ >> 15) & 0x1f;
-    register2_ = (command_ >> 20) & 0x1f;
-    immediate_ =
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.reg_control.register2 = (command_ >> 20) & 0x1f;
+    result.exe_control.immdiate =
         int(((((command_ >> 25) & 0x7f) << 5) | ((command_ >> 7) & 0x1f))
             << 20) >>
         20;
+    result.mem_control.type = Control::MEMControlInfo::MemType::STORE;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
     switch (func3_) {
     case 0x0:
-      name_ = Name::SB;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::BYTE;
       break;
     case 0x1:
-      name_ = Name::SH;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::HALF;
       break;
     case 0x2:
-      name_ = Name::SW;
+      result.mem_control.size = Control::MEMControlInfo::MemSize::WORD;
       break;
     default:
       assert(false);
@@ -173,34 +179,36 @@ void Instruction::parse() {
     break;
   }
   case 0x63: {
-    type_ = 'B';
-    register1_ = (command_ >> 15) & 0x1f;
-    register2_ = (command_ >> 20) & 0x1f;
-    func3_ = (command_ >> 12) & 0x7;
-    immediate_ =
+    result.reg_control.register1 = (command_ >> 15) & 0x1f;
+    result.reg_control.register2 = (command_ >> 20) & 0x1f;
+    result.exe_control.jump = has_jump_
+                                  ? Control::EXEControlInfo::JumpType::BYES
+                                  : Control::EXEControlInfo::JumpType::BNO;
+    result.exe_control.immdiate =
         int(((((command_ >> 7) & 0x1) << 11) | (((command_ >> 8) & 0xf) << 1) |
              (((command_ >> 25) & 0x3f) << 5) |
              (((command_ >> 31) & 0x1) << 12))
             << 19) >>
         19;
+    unsigned int func3_ = (command_ >> 12) & 0x7;
     switch (func3_) {
     case 0x0:
-      name_ = Name::BEQ;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::EQ;
       break;
     case 0x1:
-      name_ = Name::BNE;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::NE;
       break;
     case 0x4:
-      name_ = Name::BLT;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LT;
       break;
     case 0x5:
-      name_ = Name::BGE;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::GE;
       break;
     case 0x6:
-      name_ = Name::BLTU;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::LTU;
       break;
     case 0x7:
-      name_ = Name::BGEU;
+      result.exe_control.type = Control::EXEControlInfo::CalcType::GEU;
       break;
     default:
       assert(false);
@@ -208,20 +216,23 @@ void Instruction::parse() {
     break;
   }
   case 0x37: {
-    type_ = 'U';
-    rd_ = (command_ >> 7) & 0x1f;
-    immediate_ = int(command_ & 0xfffff000);
-    name_ = Name::LUI;
+    result.exe_control.type = Control::EXEControlInfo::CalcType::PRINTIMM;
+    result.exe_control.signImmediate = true;
+    result.exe_control.immdiate = int(command_ & 0xfffff000) << 12;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
     break;
   }
   case 0x17: {
-    type_ = 'U';
-    rd_ = (command_ >> 7) & 0x1f;
-    immediate_ = int(command_ & 0xfffff000);
-    name_ = Name::AUIPC;
+    result.exe_control.type = Control::EXEControlInfo::CalcType::ADD;
+    result.exe_control.signImmediate = true;
+    result.exe_control.immdiate = int(command_ & 0xfffff000) << 12;
+    result.wb_control.allow = true;
+    result.wb_control.rd = (command_ >> 7) & 0x1f;
     break;
   }
   case 0x6f: {
+    /*
     type_ = 'J';
     rd_ = (command_ >> 7) & 0x1f;
     immediate_ =
@@ -231,10 +242,10 @@ void Instruction::parse() {
             << 11) >>
         11;
     name_ = Name::JAL;
-    break;
+    break;*/
   }
   default: {
-    assert(0);
+    assert(false);
   }
   }
 }
@@ -248,32 +259,4 @@ InsBoard::InsBoard() {
 InsBoard &InsBoard::getInstance() {
   static InsBoard instance;
   return instance;
-}
-
-void InsBoard::insLaunch(Instruction &target) {
-  target.parse();
-  std::unique_lock<std::shared_mutex>(RWlock);
-  instructions_[0] = target;
-}
-
-Instruction InsBoard::readExecute() {
-  std::shared_lock<std::shared_mutex>(RWlock);
-  return instructions_[1];
-}
-
-Instruction InsBoard::readMEM() {
-  std::shared_lock<std::shared_mutex>(RWlock);
-  return instructions_[2];
-}
-
-Instruction InsBoard::readWB() {
-  std::shared_lock<std::shared_mutex>(RWlock);
-  return instructions_[3];
-}
-
-void InsBoard::refreshStage() {
-  std::unique_lock<std::shared_mutex>(RWlock);
-  for (int i = 3; i > 0; --i) {
-    instructions_[i] = instructions_[i - 1];
-  }
 }
--- src/control.hpp
+++ src/control.hpp
@@ -1,14 +1,35 @@
 #ifndef CONTROL_HPP
 #define CONTROL_HPP
+#include "mySTL.hpp"
 #include <mutex>
 #include <shared_mutex>
 
+/*
+NOP: empty command.
+R-type:ADD,SUB,SLT,SLTU,SLL,SRL,SRA,XOR,OR,AND;
+I-type:
+  ADDI,SLTI,SLTIU,SLLI,SRLI,SRAI,XORI,ORI,ANDI;
+  LW,LH,LHU,LB,LBU;
+  JALR;
+U-type:
+  LUI,AUIPC;
+S-type:
+  SW,SH,SB;
+B-type:
+  BEQ,BNE,BGE,BLT,BLTU,BGEU;
+J-type:
+  JAL;
+*/
 namespace Control {
-
-struct FixedControlInfo {
+struct AllControlInfo {
+  RegControlInfo reg_control;
+  EXEControlInfo exe_control;
+  MEMControlInfo mem_control;
+  WBControlInfo wb_control;
+};
+struct RegControlInfo {
   int register1 = 0;
   int register2 = 0;
-  int immdiate = 0;
 };
 
 struct EXEControlInfo {
@@ -17,8 +38,28 @@ struct EXEControlInfo {
   the source of operands imm-regs or immdiate; forward: 00-regs, 01-EXE_MEM
   forward, 02-MEM-WB forward.
   */
-  enum class CalcType { ADD, SUB, SLL, SLT, SLTU, SRL, SRA, XOR, OR, AND };
+  enum class CalcType {
+    ADD,
+    SUB,
+    SLL,
+    LT,
+    GE,
+    LTU,
+    GEU,
+    EQ,
+    NE,
+    SRL,
+    SRA,
+    XOR,
+    OR,
+    AND,
+    PRINTIMM
+  };
+  enum class JumpType { BYES, BNO, JALR, NOP };
   CalcType type = CalcType::ADD;
+  int immdiate = 0;
+  JumpType jump = JumpType::NOP;
+  int pc = 0;
   int signForward1 = 0;
   int signForward2 = 0;
   bool signImmediate = false;
@@ -30,111 +71,51 @@ struct MEMControlInfo {
   extend shows whether the result need to be SLL. The size distiguishes the
   result length: Byte/Half/Words.
   */
-  int type = 0;
-  int size = 0;
-  bool sign = false;
+  enum class MemSize { BYTE, HALF, WORD };
+  enum class MemType { LOAD, STORE, NOP };
+  MemSize size = MemSize::WORD;
+  MemType type = MemType::NOP;
+  bool extend_sign = false;
 };
 
-struct WBControl {
+struct WBControlInfo {
   bool allow = false;
+  int rd = 0;
 };
 }; // namespace Control
-class Instruction {
-public:
-  enum class Name {
-    /*
-    NOP: empty command.
-    R-type:ADD,SUB,SLT,SLTU,SLL,SRL,SRA,XOR,OR,AND;
-    I-type:
-      ADDI,SLTI,SLTIU,SLLI,SRLI,SRAI,XORI,ORI,ANDI;
-      LW,LH,LHU,LB,LBU;
-      JALR;
-    U-type:
-      LUI,AUIPC;
-    S-type:
-      SW,SH,SB;
-    B-type:
-      BEQ,BNE,BGE,BLT,BLTU,BGEU;
-    J-type:
-      JAL;
-    */
-    NOP,
-    ADD,
-    SUB,
-    SLT,
-    SLTU,
-    SLL,
-    SRL,
-    SRA,
-    XOR,
-    OR,
-    AND,
-    ADDI,
-    SLTI,
-    SLTIU,
-    SLLI,
-    SRLI,
-    SRAI,
-    XORI,
-    ORI,
-    ANDI,
-    LW,
-    LH,
-    LHU,
-    LB,
-    LBU,
-    JALR,
-    LUI,
-    AUIPC,
-    SW,
-    SH,
-    SB,
-    BEQ,
-    BNE,
-    BGE,
-    BLT,
-    BLTU,
-    BGEU,
-    JAL,
-  };
 
+class Instruction {
 private:
   unsigned int command_;
-  char type_;
-  Name name_;
-  unsigned int operand_;
-  unsigned int func3_;
-  unsigned int func7_;
-  unsigned int register1_;
-  unsigned int register2_;
-  unsigned int rd_;
-  int immediate_;
+  unsigned int pc_next_;
+  bool has_jump_;
 
 public:
   Instruction();
   Instruction(unsigned int command);
-  std::pair<char, Name> getType();
-  std::pair<unsigned int, unsigned int> getRegs();
-  unsigned int getRd();
-  int getImmdiate();
-  void parse();
+  Control::AllControlInfo parse();
 };
 
 class InsBoard {
 private:
-  Instruction instructions_[4];
+  // remember to fill them before start.
+  sjtu::queue<Control::RegControlInfo, 4> reg_control_;
+  sjtu::queue<Control::EXEControlInfo, 4> exe_control_;
+  sjtu::queue<Control::MEMControlInfo, 4> mem_control_;
+  sjtu::queue<Control::WBControlInfo, 4> wb_control_;
   std::shared_mutex rwlock_;
   InsBoard();
 
 public:
   static InsBoard &getInstance();
-  void insLaunch(Instruction &target);
-  Instruction readExecute();
-  Instruction readMEM();
-  Instruction readWB();
+  void IR(Instruction &target);
+  void stallPipeLine();
+  void flushPipeLine();
   void refreshStage();
+  Control::EXEControlInfo readEXEControl();
+  Control::MEMControlInfo readMEMControl();
+  Control::WBControlInfo readWBControlInfo();
+  Control::RegControlInfo readRegControl();
 };
-
-// namespace Control
 
 #endif
