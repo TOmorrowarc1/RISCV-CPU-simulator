ddd360c7923b31ef893aa11a7eb444b2a6b805b8
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-18 10:47:33 +0800

    0

--- src/storage.hpp
+++ src/storage.hpp
@@ -57,9 +57,9 @@ extern StatusRegister ID_EX_reg_2; // for register2
 extern StatusRegister EXE_MEM_reg_alu;        // for the oprand of next stages
 extern StatusRegister EXE_MEM_reg_reg2;       // for register2 which may store
 extern StatusRegister EXE_IF_reg_branch;      // for possible branch miss
-extern StatusRegister EXE_IF_reg_pc;          // for BPU refresh: the pc.
-extern StatusRegister EXE_IF_reg_target_addr; // for BPU refresh: the pc.
-extern StatusRegister EXE_IF_reg_if_jump;     // for BPU refresh: the pc.
+extern StatusRegister EXE_IF_reg_pc;          // for BPU refresh.
+extern StatusRegister EXE_IF_reg_target_addr; // for BPU refresh.
+extern StatusRegister EXE_IF_reg_if_jump;     // for BPU refresh.
 
 extern StatusRegister MEM_WB_reg_1; // for result need be written back
 
