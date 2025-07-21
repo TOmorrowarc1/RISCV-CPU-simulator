a9eec9f96180b0c18e0d95c1ca21f1103e3f3f3b
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-21 20:35:27 +0800

    feat: add stage register groups

---
+++ init.cpp
@@ -0,0 +1,3 @@
+#include"init.hpp"
+
+void Init(){}
--- naive.cpp
+++ naive.cpp
@@ -7,6 +7,15 @@ uint32_t clk = 0;
 uint32_t pc = 0;
 uint32_t registers[32] = {0};
 uint32_t memory[MEMSIZE] = {0};
+uint32_t Fetch_Issue_ins = 0;
+uint32_t Fetch_Issue_pc = 0;
+Control::ControlInfo controlInfo;
+uint32_t Execute_result;
+
+void StageIF();
+void StageIssue();
+void StageExecute();
+void StageCommit();
 
 int main() {
   Init();
@@ -17,4 +26,17 @@ int main() {
     StageExecute();
     StageCommit();
   }
+}
+
+void StageIF(){
+
+}
+void StageIssue(){
+
+}
+void StageExecute(){
+
+}
+void StageCommit(){
+  
 }
