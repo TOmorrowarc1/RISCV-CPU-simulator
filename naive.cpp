dc3ee10ea154e1e7bb4df1f3d78972ea3a6adefa
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-21 20:22:01 +0800

    feat: build the construct of naive.

--- naive.cpp
+++ naive.cpp
@@ -2,8 +2,19 @@
 #include "init.hpp"
 #include <iostream>
 
-
 const int MEMSIZE = 10000;
+uint32_t clk = 0;
 uint32_t pc = 0;
 uint32_t registers[32] = {0};
 uint32_t memory[MEMSIZE] = {0};
+
+int main() {
+  Init();
+  while (true) {
+    ++clk;
+    StageIF();
+    StageIssue();
+    StageExecute();
+    StageCommit();
+  }
+}
