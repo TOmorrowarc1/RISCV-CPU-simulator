fddbf1cfc95a75eb8181a5d0f31fd1ae879bc563
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-24 09:58:42 +0800

    feat: build RF without regs, recognizing no need to make it seperate between in CDB refresh and visit.

---
+++ src/header/RegFile.hpp
@@ -0,0 +1,28 @@
+#ifndef REGFILE_HPP
+#define REGFILE_HPP
+#include "utils.hpp"
+
+/*status refreshed by ROB is in start of the circle, while the ins changes it
+ * after one circle.*/
+struct RegFileItem {
+  uint32_t value = 0;
+  uint32_t ins_index = 0;
+  bool busy = false;
+};
+
+class RegFile {
+private:
+  RegFileItem storage[32];
+  bool flush_flag = false;
+  RegFile() = default;
+
+public:
+  static RegFile &getInstance();
+  BusyValue tryRead(uint32_t target);
+  BusyValue tryWrite(uint32_t target, uint32_t ins_index);
+  void commitReceive(ROBCommitInfo &info);
+  void flushRecieve(ROBFlushInfo &info);
+  void refresh();
+};
+
+#endif
---
+++ src/program/RegFile.cpp
@@ -0,0 +1,31 @@
+#include "header/RegFile.hpp"
+
+RegFile &RegFile::getInstance() {
+  static RegFile instance;
+  return instance;
+}
+
+BusyValue RegFile::tryRead(uint32_t target) {
+  BusyValue result;
+  result.busy = storage[target].busy;
+  result.value =
+      (result.busy) ? storage[target].ins_index : storage[target].value;
+}
+
+BusyValue RegFile::tryWrite(uint32_t target, uint32_t ins_index) {
+  BusyValue result;
+  result.busy = storage[target].busy;
+  result.value = storage[target].ins_index;
+  storage[target].busy = true;
+  storage[target].ins_index = ins_index;
+  return result;
+}
+
+void RegFile::commitReceive(ROBCommitInfo &info) {
+  storage[info.rd].value = info.value;
+  storage[info.rd].busy = !(storage[info.rd].ins_index == info.index);
+}
+
+void RegFile::flushRecieve(ROBFlushInfo &info) {}
+
+void RegFile::refresh() {}
