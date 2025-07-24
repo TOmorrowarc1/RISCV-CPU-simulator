340d8956931afb08e288948029e077f23eb1259a
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-24 16:52:20 +0800

    feat: start to deal with the executors.

--- src/header/Executor.hpp
+++ src/header/Executor.hpp
@@ -1,4 +1,54 @@
 #ifndef EXECUTE_HPP
 #define EXECUTE_HPP
+#include "utils.hpp"
+
+const uint32_t LSBSIZE = 8;
+class ALU {
+private:
+  ALU();
+
+public:
+  static ALU &getInstance();
+  BoardCastInfo execute(ALUInfo &order);
+};
+
+class BU {
+private:
+  BU();
+
+public:
+  static BU &getInstance();
+  BoardCastInfo execute(BUInfo &order);
+};
+
+struct LSBItem {
+  InsType type;
+  MemType size;
+  uint32_t oprand1;
+  uint32_t oprand2;
+  uint32_t immediate;
+  uint32_t target;
+  buffer<bool> ready = buffer<bool>(false);
+  buffer<bool> busy = buffer<bool>(false);
+  bool ready1 = false;
+  bool ready2 = false;
+};
+
+class LSB {
+private:
+  LSBItem storage[LSBSIZE];
+  buffer<uint32_t> head_;
+  buffer<uint32_t> tail_;
+  LSB();
+
+public:
+  static LSB &getInstance();
+  void newIns(DecodeInsInfo &decode, BusyValue &oprand1, BusyValue &oprand2,
+              uint32_t index);
+  void listenCDB(BoardCastInfo &info);
+  BoardCastInfo tryExecute();
+  void flushReceive(ROBFlushInfo &info);
+  void refresh();
+};
 
 #endif
---
+++ src/program/Executor.cpp
@@ -0,0 +1,66 @@
+#include "header/Executor.hpp"
+
+ALU &ALU::getInstance() {
+  static ALU instance;
+  return instance;
+}
+BoardCastInfo ALU::execute(ALUInfo &order) {
+  BoardCastInfo result;
+  result.index = order.index;
+  switch (order.type) {
+  case CalcType::ADD:
+    result.value = order.oprand1 + order.oprand2;
+    break;
+  case CalcType::SUB:
+    result.value = order.oprand1 - order.oprand2;
+    break;
+  case CalcType::SLL:
+    result.value = order.oprand1 << order.oprand2;
+    break;
+  case CalcType::SRA:
+    result.value = int(order.oprand1) >> order.oprand2;
+    break;
+  case CalcType::SRL:
+    result.value = order.oprand1 >> order.oprand2;
+    break;
+  case CalcType::LT:
+    result.value = int(order.oprand1) < int(order.oprand2);
+    break;
+  case CalcType::LTU:
+    result.value = order.oprand1 < order.oprand2;
+    break;
+  case CalcType::XOR:
+    result.value = order.oprand1 ^ order.oprand2;
+    break;
+  case CalcType::OR:
+    result.value = order.oprand1 | order.oprand2;
+    break;
+  case CalcType::AND:
+    result.value = order.oprand1 & order.oprand2;
+    break;
+  case CalcType::IMM:
+    result.value = order.oprand2;
+    break;
+  }
+  return result;
+}
+
+BU &BU::getInstance() {
+  static BU instance;
+  return instance;
+}
+BoardCastInfo BU::execute(BUInfo &order){
+  
+}
+
+LSB &LSB::getInstance() {
+  static LSB instance;
+  return instance;
+}
+
+void LSB::newIns(DecodeInsInfo &decode, BusyValue &oprand1, BusyValue &oprand2,
+                 uint32_t index);
+void LSB::listenCDB(BoardCastInfo &info);
+BoardCastInfo LSB::tryExecute();
+void LSB::flushReceive(ROBFlushInfo &info);
+void LSB::refresh();
