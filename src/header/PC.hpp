3dca01f601c15f277b85890168ce85de969f7633
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-25 10:25:19 +0800

    feat: build pc without predictor.

--- src/header/PC.hpp
+++ src/header/PC.hpp
@@ -1,4 +1,17 @@
 #ifndef PC_HPP
 #define PC_HPP
 #include "memory.hpp"
+
+class PC {
+private:
+  buffer<uint32_t> pc_;
+
+public:
+  static PC &getInstance();
+  BranchPredictInfo branchPredict();
+  BasicInsInfo fetchCommand(uint32_t address);
+  void flushReceive(ROBFlushInfo& info);
+  void refresh();
+};
+
 #endif
--- src/program/PC.cpp
+++ src/program/PC.cpp
@@ -1,1 +1,22 @@
 #include "header/PC.hpp"
+
+PC &PC::getInstance() {
+  static PC instance;
+  return instance;
+}
+
+BranchPredictInfo PC::branchPredict() {
+  BranchPredictInfo result;
+  result.branch_predict = pc_.getValue();
+  result.taken_predict = false;
+  return result;
+}
+
+BasicInsInfo PC::fetchCommand(uint32_t address) {
+  uint32_t command = Memory::getInstance().load(address, 4);
+  return {command, pc_.getValue()};
+}
+
+void PC::flushReceive(ROBFlushInfo &info) { pc_.writeValue(info.branch); }
+
+void PC::refresh() { pc_.refresh(); }
