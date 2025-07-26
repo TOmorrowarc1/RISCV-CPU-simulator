6b32946abd31c5293b4fd4984c8a5a7a56fccb5d
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-26 21:59:44 +0800

    0

--- src/header/PC.hpp
+++ src/header/PC.hpp
@@ -2,9 +2,22 @@
 #define PC_HPP
 #include "memory.hpp"
 
+const uint32_t BATSIZE = 1 << 20;
+class JumpState {
+private:
+  uint32_t status = 1;
+  uint32_t address = 0;
+
+public:
+  void refreshCondition(bool if_jump);
+  void refreshAddress(uint32_t new_addr);
+  uint32_t predict();
+};
+
 class PC {
 private:
   buffer<uint32_t> pc_;
+  JumpState predictor[BATSIZE];
   PC();
 
 public:
